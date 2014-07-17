237c368517f9cf40360ea7abb9ab029d6ea3599e
George Tsouloupas <george.tsouloupas@gmail.com>
2014-07-18 01:15:08 +0300

    Add initial models and migrate db

--- userdb/models.py
+++ userdb/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+   username = models.CharField(max_length=50)
+   fname = models.CharField(max_length=50)
+   lname = models.CharField(max_length=50)
+   project = models.ForeignKey("Project")
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    cpu_allocation = models.IntegerField(default=0)
+
