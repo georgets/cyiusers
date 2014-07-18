3c89778be4c3e6fe7a2175c58aa6e870f166b943
George Tsouloupas <george.tsouloupas@gmail.com>
2014-07-18 10:10:28 +0300

    Add multiple users in one go

--- userdb/admin.py
+++ userdb/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from userdb.models import User,Project
+
+class UsersInline(admin.TabularInline):
+    model = User
+    extra = 3
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [UsersInline]
+
+admin.site.register(User)
+admin.site.register(Project,ProjectAdmin)
 
 # Register your models here.
--- userdb/models.py
+++ userdb/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class User(models.Model):
-   username = models.CharField(max_length=50)
-   fname = models.CharField(max_length=50)
-   lname = models.CharField(max_length=50)
-   project = models.ForeignKey("Project")
+    username = models.CharField(max_length=50)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    project = models.ForeignKey("Project")
+ 
+    def __unicode__(self):
+        return "%s, %s (%s)" % (self.lname,self.fname,self.username)
 
 class Project(models.Model):
     name = models.CharField(max_length=50)
     cpu_allocation = models.IntegerField(default=0)
+  
+    def __unicode__(self):
+        return self.name
 
